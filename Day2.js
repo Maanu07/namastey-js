5d9dca06f8b5878cbe8304143fbdfc37e0bfd591
Manas Joshi <76611475+Maanu07@users.noreply.github.com>
2022-05-17 09:03:33 +0530

    updated day2.js

--- Day2.js
+++ Day2.js
@@ -1,21 +1,22 @@
-/* console.log(x)
-console.log(getName)
-getName()
+console.log(x)           // undefined
+console.log(getName)    // function body
+getName()               // prints 'Hi, my name is Manas...'
 
-var x = 7; //declaration + initialization
+var x = 7;              // declaration + initialization
 function getName(){
     console.log('Hi, my name is Manas...')
-}
+}                       //function declaration
 
-console.log(getName)
-getName() */
+console.log(x)          // 7
+console.log(getName)    // function body
+getName()               // prints 'Hi, my name is Manas...'
 
 // =============== Hoisting with arrow function ========================
-console.log(firstName)
-firstName()   // gives error 
+console.log(firstName)      // undefined
+firstName()                 // gives error 
 
 var firstName = () =>{
     console.log("Manas")
 }
 
-// 1. Hoisting in JS allow us to use variables/functions before they have been declared
+// 1. Hoisting in JS takes all variables/functions declaration to the top of the scope and initialize variables with undefined and functions with the function body..

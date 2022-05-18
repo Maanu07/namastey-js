7af7184115910a6e642e2747bebb06f40f2d16e2
Maanu07 <manasjoshi.mj6@gmail.com>
2022-05-18 14:59:43 +0530

    added day7

---
+++ Day7.js
@@ -0,0 +1,16 @@
+var a = 10;
+let b = 10
+console.log(b)
+{
+    var a = 100;
+    let b=100
+    console.log(b)
+}
+console.log(b)
+
+
+// var is either global scope or function scope
+// let and const are block scope 
+// 'block' represents set of instructions 
+// shadowing in JS is overriding a variable
+// illegal shadowing in JS 
